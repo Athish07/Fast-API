0ca0db7ff3beb0af183c5f4e27d28414593c2bd0
Athish T <athish.t@gds.ey.com>
2026-02-02 15:26:38 +0530

    completed update and delete functionality

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from asyncio import Task
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -18,7 +19,44 @@ class Todo(db.Model):
     
 @app.route('/', methods = ['GET','POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+        
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your task'
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks = tasks)
+    
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+    
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+         return 'There was a problem deleting that task'
 
+@app.route('/update/<int:id>', methods=['GET','POST'])
+def update(id):
+    task_to_update = Todo.query.get_or_404(id)
+    
+    if request.method == 'POST':
+        task_to_update.content = request.form['content']
+        
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue updating your task'
+    else:
+        return render_template('update.html',task=task_to_update)
 if __name__ == "__main__":
     app.run(debug=True, host="127.0.0.1", port=8000)
